e65b48726863e5014bc66d70751ce841a5a5999e
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2019-03-21 22:31:46 +0900

    json to block

--- index.js
+++ index.js
@@ -14,6 +14,14 @@ const makeWorkspace = toolbox => {
 
   workspace = Blockly.inject(blocklyArea, makeOption(toolbox));
   Blockly.svgResize(workspace);
+
+  const updateWorkspace = () => {
+    const xml = Blockly.Xml.workspaceToDom(workspace);
+    const xmlText = Blockly.Xml.domToText(xml);
+    console.log(xmlText);
+  };
+
+  workspace.addChangeListener(updateWorkspace);
 };
 
 const makeOption = toolbox => {
@@ -32,6 +40,55 @@ const makeOption = toolbox => {
   };
 };
 
+// make block functions
+const startBlock = connectedBlock => {
+  return `<xml xmlns="http://www.w3.org/1999/xhtml"><block type="start"><next>${connectedBlock}</next></block></xml>`;
+};
+
+const conditionBlock = (conditionText, yesBlock, noBlock) => {
+  return `<block type="condition"><field name="condition_name">${conditionText}</field><statement name="yes">${yesBlock}</statement><statement name="no">${noBlock}</statement></block>`;
+};
+
+const targetBlock = text => {
+  return `<block type="target"><field name="target_name">${text}</field></block>`;
+};
+
+const parse = json => {
+  if (json.isCondition) {
+    const yesBlock = parse(json.yes);
+    const noBlock = parse(json.no);
+    return conditionBlock(json.link, yesBlock, noBlock);
+  }
+  return targetBlock(json.link);
+};
+
+const initBlock = () => {
+  const inputExample = {
+    link: 1,
+    isCondition: true,
+    yes: {
+      link: 2,
+      isCondition: false
+    },
+    no: {
+      link: 3,
+      isCondition: true,
+      yes: {
+        link: 4,
+        isCondition: false
+      },
+      no: {
+        link: 5,
+        isCondition: false
+      }
+    }
+  };
+
+  const xmlText = startBlock(parse(inputExample));
+  const xml = Blockly.Xml.textToDom(xmlText);
+  Blockly.Xml.domToWorkspace(xml, workspace);
+};
+
 (async () => {
   const requestUrl = ["/toolbox.xml"];
 
@@ -39,4 +96,5 @@ const makeOption = toolbox => {
 
   const htmlToolbox = result[0];
   makeWorkspace(htmlToolbox);
+  initBlock();
 })();
